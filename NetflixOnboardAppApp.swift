2530d4bbb94c83356adcb63a91f4aab6fdb6398c
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-22 17:59:25 +0100

    Initial commit

---
+++ NetflixOnboardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixOnboardAppApp.swift
+//  NetflixOnboardApp
+//
+//  Created by Nicola Kaleta on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixOnboardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Views/LottieView.swift
@@ -0,0 +1,10 @@
+//
+//  LottieView.swift
+//  NetflixOnboardApp
+//
+//  Created by Nicola Kaleta on 22/02/2024.
+//
+
+import SwiftUI
+import Lottie
+
